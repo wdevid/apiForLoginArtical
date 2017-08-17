2bdb205ea726271ebda92d1ebbc733c623889038
wdevid <821017115@qq.com>
2017-08-17 13:01:57 +0800

    数据库操作的优化

--- controllers/circlestory.go
+++ controllers/circlestory.go
@@ -13,14 +13,22 @@ type CircleControllers struct {
 
 func (this *CircleControllers) Post(){
 	a,err := this.GetInt("page",-1)
-	if err!=nil {
+	if err==nil {
 		o := orm.NewOrm()
 		o.Using("myapp")
-		c := new(models.Customer)
-		qs := o.QueryTable(&c).Filter("id__in",a,a+20)
-		if qs != nil {
-			this.Data["json"],err = json.Marshal(&c)
+		customers := make([]*models.Customer,20)
+		ids := make([]int64,a,a+20)
+		for _,num :=range ids{
+			cus := new(models.Customer)
+			cus.Id = num
+			err := o.Read(cus)
+			if err!=nil {
+				this.Data["json"] = "{\"circle\":\"没有更多数据!!!\"}"
+			}else {
+				customers[num] = cus
+			}
 		}
+		this.Data["json"],err = json.Marshal(&customers)
 
 	}else {
 		this.Data["json"] = "{\"circle\":\"没有更多数据!!!\"}"
